6780339a7aba0e68cd77641492e368eb8b2f2bb8
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-15 20:39:49 +0100

    Add failing test to ensure ages 4-12 is kid

--- tests.mjs
+++ tests.mjs
@@ -9,20 +9,19 @@ describe("age calculator", () => {
     // assert
     assert.equal(result, 50);
   });
-});
-
-it("someone born 2021 is 1 2022", () => {
-  // arrange
-  // act
-  const result = getAge(2021, 2022);
-
-  // assert
-  assert.equal(result, 1);
-});
-
-it("I am 27 years old in 2025", () => {
-  const result = getAge(1998, 2025);
-  assert.equal(result, 27);
+  it("someone born 2021 is 1 2022", () => {
+    // arrange
+    // act
+    const result = getAge(2021, 2022);
+  
+    // assert
+    assert.equal(result, 1);
+  });
+  
+  it("I am 27 years old in 2025", () => {
+    const result = getAge(1998, 2025);
+    assert.equal(result, 27);
+  });
 });
 
 describe("age classifier", () => {
@@ -34,7 +33,14 @@ describe("age classifier", () => {
     // assert
     assert.equal(result, "toddler");
   });
-  it("4-12 years old should be a kid");
+  it("4-12 years old should be a kid", () => {
+    // arrange
+    // act
+    const result = getAgeGroup(4);
+  
+    // assert
+    assert.equal(result, "kid");
+  });
   it("13-19 years old should be a teenager");
   it("20-39 years old should be a adult");
   it("above 39 years old ... you are just old");
