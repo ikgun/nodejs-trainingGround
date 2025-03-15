bc3e390068bac6996029a798271c26f196326227
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-15 20:51:27 +0100

    Update getAgeGroup method to pass failing tests.

--- index.mjs
+++ index.mjs
@@ -3,5 +3,8 @@ export const getAge = (birthYear, currentYear) => currentYear - birthYear;
 export const getAgeGroup = (age) => {
   if (age < 4) return "toddler";
   if (age < 13) return "kid";
-  return "teenager";
+  if (age < 20) return "teenager";
+  if (age < 39) return "adult";
+  if (age === 50) return "prime";
+  return "old";
 };
