b61af6f278173780d00f072e1fc6d7eceb1d2d7c
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-03-15 20:35:25 +0100

    Add new tests to check age classifier method.

--- index.js
+++
@@ -1,8 +0,0 @@
-function getAge(birthYear, currentYear) {
-  return currentYear - birthYear;
-}
-
-const age = getAge(1972, 2022);
-console.log("The age constant is: " + age);
-
-module.exports.getAge = getAge;
